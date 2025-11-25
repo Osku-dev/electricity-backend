13ce49df26248942e7081ec094de7b79c9e3e74f
oskah <oskariheikkinen@hotmail.com>
2025-11-25 13:13:05 +0200

    fixed a mistake in webconfig

--- src/main/java/com/example/electricity_backend/config/WebConfig.java
+++ src/main/java/com/example/electricity_backend/config/WebConfig.java
@@ -1,17 +1,22 @@
 package com.example.electricity_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${FRONTEND_URL}")
+private String frontendUrl;
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/graphql/**")
-                .allowedOrigins("${FRONTEND_URL}")
+        registry.addMapping("/graphql")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
