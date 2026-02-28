5cb2fde2a112eaf2eff5ab43a266bf406ca35630
oskah <oskariheikkinen@hotmail.com>
2026-02-28 13:48:25 +0200

    modifying migration file before possible deployment

--- src/main/resources/db/migration/V1__create_price_table.sql
+++ src/main/resources/db/migration/V1__create_price_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE price (
     id BIGINT PRIMARY KEY,
     start_time TIMESTAMPTZ NOT NULL,
     price_cents NUMERIC(10,3) NOT NULL,
-    resolution_minutes INTEGER NOT NULL DEFAULT 60
+    resolution_minutes INTEGER NOT NULL DEFAULT 15
 );
